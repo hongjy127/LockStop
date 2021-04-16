c556c63ba94feb850e023dd0d373f6640e1a6211
Ryu <rjh7693@gmail.com>
2021-04-16 17:50:52 +0900

    DB_Pc_design

--- DB(Pc_Server)/webServer/recordProject/record/views.py
+++ DB(Pc_Server)/webServer/recordProject/record/views.py
@@ -1,6 +1,6 @@
-from record.models import Lockdata, Test
+from record.models import Lockdata
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -11,14 +11,20 @@ from django.views.generic import ListView, TemplateView, DetailView
 class TestLV(ListView):
     model = Lockdata
     template_name = 'record/record.html' # 템플릿 파일명 변경
-    # context_object_name = 'object_list' # 컨텍스트 객체 이름 변경(object_list)
-    # paginate_by = 10 # 페이지네이션, 페이지당 문서 건 수
+    context_object_name = 'object_list' # 컨텍스트 객체 이름 변경(object_list)
+    paginate_by = 7 # 페이지네이션, 페이지당 문서 건 수(
+    
+    
 
     # def get_context_data(self,**kwargs):  
     #     context = super().get_context_data(**kwargs)
     #     context['object_list'] = Lockdata.objects.all()
-        
     #     return context
+
+    # lockdata = Lockdata.objects.all()
+    # Lockdata.objects.create(topic='토픽',value='값')
+    # Lockdata.objects.filter(topic__contains="doorlock")
+
 
 
 class TestDV(DetailView):
